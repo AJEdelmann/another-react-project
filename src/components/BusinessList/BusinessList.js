0bc198986efc3ab63d95c5140465ad3d44921abd
ajedelmann <albert.edelmann@gmail.com>
2019-12-15 23:05:47 +0100

    created business array and set BusinessList prop

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,18 +6,7 @@ class BusinessList extends React.Component {
     render() {
         return ( <
             div className = "BusinessList" >
-            <
-            Business / >
-            <
-            Business / >
-            <
-            Business / >
-            <
-            Business / >
-            <
-            Business / >
-            <
-            Business / >
+
             <
             /div>
         )
