4cf0f1fa23fc9f195cbb667ddb26d4599f102060
ajedelmann <albert.edelmann@gmail.com>
2019-12-17 22:35:09 +0100

    bind methods in the constructor and handle term and location onChange

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,6 +9,8 @@ class SearchBar extends React.Component {
       location: "",
       sortBy: "best_match"
     };
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
     this.sortByOptions = {
       "Best Match": "best_match",
       "Highest Rated": "rating",
@@ -54,8 +56,11 @@ class SearchBar extends React.Component {
           <ul> {this.renderSortByOptions()} </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input
+            onChange={this.handleTermChange}
+            placeholder="Search Businesses"
+          />
+          <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
           <a>Let 's Go</a>
